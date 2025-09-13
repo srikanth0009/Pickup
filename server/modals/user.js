236eee950a12738df2a9274e539ca2d4b4a49ff8
srikanth <srikanthchowdary8499@gmail.com>
2025-09-13 13:16:06 +0530

    commit changes

--- server/modals/user.js
+++ server/modals/user.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const  userSchema = new mongoose.Schema({
     _id: {type: String, required: true},
     email: {type: String, required: true},
-    fumm_name: {type: String, required: true},
+    full_name: {type: String, required: true},
     username: {type: String, unique: true},
     bio: {type: String, default: 'Hey there! I am using Pingup'},
     profile_picture: {type: String, default: ''},
